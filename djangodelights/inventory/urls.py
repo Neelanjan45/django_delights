ccad75d8b1cd383c5c56787f2a3b4de66fe494b8
NilanjanAdak <niladak94@gmail.com>
2025-02-15 11:50:41 +0530

    Ingredient delete and home view created

--- djangodelights/inventory/urls.py
+++ djangodelights/inventory/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("ingredient/list", views.IngredientList.as_view(), name="ingredients"),
+    path("ingredient/delete/<pk>", views.IngredientDelete.as_view(), name="ingredientdelete"),
     path("menu/list", views.MenuItemList.as_view(), name="menuitems"),
     path("purchase/list", views.PurchaseList.as_view(), name="purchases"),
 ]
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Ingredient, MenuItem, Purchase
+
+def home(request):
+    # Profit and revenue view
+    return render(request, "inventory/home.html")
 
 class IngredientList(ListView):
     model = Ingredient
     template_name = "inventory/ingredient_list.html"
+
+class IngredientDelete(DeleteView):
+    model = Ingredient
+    template_name = "inventory/ingredient_delete_form.html"
     
 class MenuItemList(ListView):
     model = MenuItem
