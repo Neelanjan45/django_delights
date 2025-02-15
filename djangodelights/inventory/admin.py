ad924cd55a2c2625e48406f5319747ffaa0be910
Nilanjan Adak <niladak94@gmail.com>
2025-02-15 11:59:20 +0530

    Revert "Feature/dd create views"

--- djangodelights/inventory/admin.py
+++ djangodelights/inventory/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
-admin.site.register(Ingredient)
-admin.site.register(MenuItem)
-admin.site.register(RecipeRequirement)
-admin.site.register(Purchase)
+# Register your models here.
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,25 +1,3 @@
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render
 
-from .models import Ingredient, MenuItem, Purchase
-
-def home(request):
-    # Profit and revenue view
-    return render(request, "inventory/home.html")
-
-class IngredientList(ListView):
-    model = Ingredient
-    template_name = "inventory/ingredient_list.html"
-
-class IngredientDelete(DeleteView):
-    model = Ingredient
-    template_name = "inventory/ingredient_delete_form.html"
-    
-class MenuItemList(ListView):
-    model = MenuItem
-    template_name = "inventory/menu_item_list.html"
-
-class PurchaseList(ListView):
-    model = Purchase
-    template_name = "inventory/purchase_list.html"
+# Create your views here.
